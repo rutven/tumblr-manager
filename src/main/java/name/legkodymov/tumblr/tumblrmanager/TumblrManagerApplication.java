3cf575172bc2833fa5c5b5db4158057e4c36c7ec
Sergei Legkodymov <rutven@gmail.com>
2017-08-11 16:19:53 +0300

    Spring Boot init

---
+++ src/main/java/name/legkodymov/tumblr/tumblrmanager/TumblrManagerApplication.java
@@ -0,0 +1,12 @@
+package name.legkodymov.tumblr.tumblrmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TumblrManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TumblrManagerApplication.class, args);
+	}
+}
